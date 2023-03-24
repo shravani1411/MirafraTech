87008c49600eda7e2d0c87cf23dbd7fd4e5628cd
shravani_jorrigala <shravanijorrigala821@gmail.com>
2023-03-24 14:18:52 +0530

    storage_classes

---
+++ C_practice/add.c
@@ -0,0 +1,10 @@
+//Addition
+
+#include<stdio.h>
+int addition()
+{
+	int c;
+	extern int x,y;
+	c=x+y;
+}
+
---
+++ C_practice/main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int addition();
+int x=1,y=2;
+int main()
+{
+	int c;
+	c=addition();
+	printf("%d\n",c);
+}
+
